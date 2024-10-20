227e12e956ec0431a82b9a3bd8fcbb3fe32b33ed
f4tboiz07 <bmozsik@gmail.com>
2024-10-20 17:57:29 +0200

    kész

--- 2feladat.py
+++ 2feladat.py
@@ -1,12 +1,16 @@
 # 1. Beolvasás
 file_name = 'ki.txt'
 
+def prGreen(szoveg): print("\033[92m {}\033[00m".format(szoveg))
+#def prYellow(szoveg): print("\033[93m {}\033[00m".format(szoveg))
+def prRed(szoveg): print("\033[91m {}\033[00m".format(szoveg))
+
 try:
     with open(file_name, 'r', encoding='utf-8') as f:
         data = f.read().split(';')  # A ';' karakter alapján választjuk szét az adatokat
         data = [item.strip() for item in data]  # Eltávolítjuk a felesleges szóközöket
 except FileNotFoundError:
-    print(f"A {file_name} fájl nem található!")
+    prRed(f"File {file_name} not found!")
     data = None
 
 if data is not None:
@@ -16,12 +20,12 @@ if data is not None:
     elif all(item.isalpha() for item in data):
         adat_tipus = 'szovegek'
     else:
-        print("Hiba: A fájl vegyesen tartalmaz számokat és szövegeket, vagy helytelen formátumú adatokat!")
+        prRed("Error: The file contains a mix of numbers and text, or data in an incorrect format!")
         adat_tipus = None
 
     if adat_tipus is not None:
         # 3. Rendezési irány kiválasztása
-        irany = input("Kérem válassza ki a rendezés irányát (n - növekvő, c - csökkenő): ").lower()
+        irany = input("Please select the sorting order (a - ascending, d - descending): ").lower()
         novekvo = True if irany == 'n' else False
 
         # 4. Rendezés elvégzése
@@ -48,10 +52,10 @@ if data is not None:
                         if len(data[i]) < len(data[j]) or (len(data[i]) == len(data[j]) and data[i] < data[j]):
                             data[i], data[j] = data[j], data[i]
 
-        print("Rendezett lista:", data)
+        prGreen("Ordered list: ", data)
 
         # 5. Új elem beillesztése
-        uj_elem = input("Adjon meg egy új elemet (szám vagy szöveg): ")
+        uj_elem = input("Please enter a new item (number or text): ")
 
         if adat_tipus == 'szamok':
             if uj_elem.isdigit():
@@ -68,7 +72,7 @@ if data is not None:
                             if data[i] < data[j]:
                                 data[i], data[j] = data[j], data[i]
             else:
-                print("Hiba: A megadott új elem nem szám!")
+                prRed("Error: The specified new item is not a number!")
         else:
             if uj_elem.isalpha():
                 data.append(uj_elem)
@@ -83,14 +87,14 @@ if data is not None:
                             if len(data[i]) < len(data[j]) or (len(data[i]) == len(data[j]) and data[i] < data[j]):
                                 data[i], data[j] = data[j], data[i]
             else:
-                print("Hiba: A megadott új elem nem szöveg!")
+                prRed("Error: The specified new item is not text!")
 
-        print("Új rendezett lista:", data)
+        prGreen("New ordered list: ", data)
 
         # 6. Visszaírás a fájlba
         try:
             with open(file_name, 'w', encoding='utf-8') as f:
                 f.write(';'.join(str(item) for item in data))  # A listát ';' karakterrel választjuk el
-            print(f"A rendezett lista sikeresen kiírva a {file_name} fájlba!")
+            prGreen(f"The sorted list has been successfully written to the {file_name} file!")
         except Exception as e:
-            print(f"Hiba történt a fájl írásakor: {e}")
+            prRed(f"An error occurred while writing the file: {e}")
